b44ba670e9e99e9fb9ee71c7c64a6d9c48a3a56d
sinmunsu <ace05227@gmail.com>
2021-11-03 21:53:39 +0900

    내정보 시작

--- lib/sidebar/ScreenI.dart
+++ lib/sidebar/ScreenI.dart
@@ -10,12 +10,31 @@ class ScreenI extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
-      body: Center(
-        child: Text(
-          '내 정보',
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ),
-    );
+      body: ListView(
+        padding: const EdgeInsets.all(15.0),
+        children: [
+          Container(
+            height: 300,
+            color: Colors.grey,
+            child: Column(
+              children: [
+                Container(
+                  child: Text('내정보', style: TextStyle(fontSize: 20, color: Colors.black),),
+                ),
+                Text('신문수')
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Text('계정', style: TextStyle(fontSize: 20, color: Colors.black)),
+          ),
+          Container(
+            height: 300,
+            color: Colors.grey,
+          ),
+        ]
+      )
+      );
   }
 }
