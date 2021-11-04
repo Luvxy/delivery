8204340129bfdcb78107d93901c2706fc2ff2165
Luvxy <93637069+Luvxy@users.noreply.github.com>
2021-11-04 13:46:39 +0900

    Delete some side button make detail1 detail1 = 주문상세 detail2 = 배달 주문 상세
    detail1 틀 완성
    detail2 틀 완성
    
    주문 상세 완성 틀 완성
    배달 대기 목록 틀 완성

--- lib/detail1.dart
+++ lib/detail1.dart
@@ -19,8 +19,35 @@ class detail1 extends StatelessWidget {
                 color: Colors.grey,
                 child: Column(
                   children: [
-                    Container(
-                      child: Text('물건이름 : 실내화', style: TextStyle(fontSize: 20, color: Colors.white),),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('배달 정보', style: TextStyle(fontSize: 30, color: Colors.white),),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('물건이름 : 실내화', style: TextStyle(fontSize: 20, color: Colors.white),),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('장소 : 오석관 -> 벧엘관', style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('가격 : 1000 원', style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('결제수단 : 카드', style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
                     ),
                   ],
                 ),
@@ -31,6 +58,41 @@ class detail1 extends StatelessWidget {
               Container(
                 height: 300,
                 color: Colors.grey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('배달원 정보', style: TextStyle(fontSize: 30, color: Colors.white)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('배달원 닉네임 : 하늘에달린소', style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text('배달 완료 시간 : 23:59', style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: Text(''),
+                      ),
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        color: Colors.red,
+                        child: Text('신고하기',style: TextStyle(fontSize: 20, color: Colors.white)),
+                        onPressed: (){},
+                      ),
+                    ),
+                  ]
+                ),
               ),
             ],
         ),
